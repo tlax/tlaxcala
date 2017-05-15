02ef6259d4273ae9dfccde4acd94fe7d2b2a25e3
vauxhall <zero.griffin@gmail.com>
2017-05-15 11:46:02 -0500

    view home bar

--- Tlaxcala/View/Home/VHome.swift
+++ Tlaxcala/View/Home/VHome.swift
@@ -3,11 +3,17 @@ import UIKit
 class VHome:VView
 {
     private weak var controller:CHome!
+    private weak var viewBar:VHomeBar!
     
     override init(controller:CController)
     {
         super.init(controller:controller)
         self.controller = controller as? CHome
+        
+        let viewBar:VHomeBar = VHomeBar(controller:self.controller)
+        self.viewBar = viewBar
+        
+        addSubview(viewBar)
     }
     
     required init?(coder:NSCoder)
